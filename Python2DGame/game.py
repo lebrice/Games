71b41ae54df427abadadc3a08a3bbca508fb81f9
Fabrice Normandin <fabrice.normandin@gmail.com>
2018-10-22 21:47:11 -0400

    All in one file, screw Python packages
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- Python2DGame/game.py
+++ Python2DGame/game.py
@@ -1,13 +1,200 @@
+import arcade
+import numpy as np
 from typing import NamedTuple, List, Tuple, Set, Dict
 from midpoint_bisection import Point
 import numpy as np
 import random
-import testing
 import os
 import arcade
 from enum import Enum
 # from testing import SCREEN_HEIGHT, SCREEN_WIDTH, GROUND_Y, WALL_X
 import collision_detection
+from midpoint_bisection import midpoint_bisection, Point
+import arcade
+import random
+import os
+import math
+import os
+
+FRAME_RATE = 30
+FRAME_TIME = 1.0 / FRAME_RATE
+
+GRAVITY = -9.81
+
+NUM_ITERATIONS = 1
+
+# Set the working directory (where we expect to find files) to the same
+# directory this .py file is in. You can leave this out of your own
+# code, but it is needed to easily run the examples using "python -m"
+# as mentioned at the top of this program.
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+IMAGE_DIR = os.path.join(os.path.curdir, "images")
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+GROUND_Y = SCREEN_HEIGHT / 6
+MOUNTAIN_WIDTH = SCREEN_WIDTH / 3
+MOUNTAIN_X_MIN = SCREEN_WIDTH / 3
+MOUNTAIN_X_MAX = MOUNTAIN_X_MIN + MOUNTAIN_WIDTH
+MOUNTAIN_Y_MAX = SCREEN_HEIGHT / 2
+MOUNTAIN_Y_MIN = GROUND_Y
+MOUNTAIN_HEIGHT = MOUNTAIN_Y_MAX - MOUNTAIN_Y_MIN
+WALL_X = 0
+
+CANNON_X = SCREEN_WIDTH * 7/8
+CANNON_Y = GROUND_Y + 10
+CLOUD_Y = (7/8) * SCREEN_HEIGHT
+
+
+WIND_START_Y: int = MOUNTAIN_Y_MAX
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+        self.mountain_points = [
+            Point(MOUNTAIN_X_MIN, MOUNTAIN_Y_MIN),
+            Point(int(MOUNTAIN_X_MIN + 0.50 * (MOUNTAIN_WIDTH)), MOUNTAIN_Y_MAX),
+            Point(MOUNTAIN_X_MAX, MOUNTAIN_Y_MIN),
+        ]
+        self.wall_points = [
+            Point(WALL_X, GROUND_Y),
+            Point(WALL_X, SCREEN_HEIGHT - 1),
+        ]
+        self.turkeys = None
+        self.particle_system: ParticleSystem = None
+        self.ball = None
+        self.clouds : arcade.SpriteList = None
+        self.cannon_angle: float = 0.
+
+    def setup(self):
+        # Create your sprites and sprite lists here
+        self.mountain_points = midpoint_bisection(self.mountain_points, max_iterations=4)
+        print([int(p.y) for p in self.mountain_points])
+        self.particle_system = ParticleSystem()
+        
+        self.clouds = arcade.SpriteList()
+        self.clouds.append(Cloud((SCREEN_WIDTH / 2, SCREEN_HEIGHT * 2/3)))
+        self.clouds.append(Cloud((SCREEN_WIDTH / 3, SCREEN_HEIGHT * 3/4)))       
+
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        arcade.start_render()
+        self.draw_background()
+        self.draw_mountain()
+        self.draw_wall()
+        self.draw_ball()
+        self.clouds.draw()
+        self.draw_cannon()
+        # Call draw() on all your sprite lists below
+
+    def update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        self.clouds.update()
+        self.particle_system.time_step()
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        http://arcade.academy/arcade.key.html
+        """
+        if key == arcade.key.DOWN:
+            self.cannon_angle -= 5
+            self.cannon_angle = max(self.cannon_angle, 0)
+        if key == arcade.key.UP:
+            self.cannon_angle += 5
+            self.cannon_angle = min(self.cannon_angle, 90)
+        if key == arcade.key.SPACE:
+            self.fire_cannonball()
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+    def draw_background(self):
+        """
+        This function draws the background. Specifically, the sky and ground.
+        """
+        # Draw the ground
+        arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, GROUND_Y / 2,
+                                    SCREEN_WIDTH - 1, GROUND_Y,
+                                    arcade.color.DARK_SPRING_GREEN)
+        # Draw the sky
+        arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, GROUND_Y + (SCREEN_HEIGHT - GROUND_Y) / 2,
+                                    SCREEN_WIDTH - 1, SCREEN_HEIGHT - GROUND_Y,
+                                    arcade.color.SKY_BLUE)
+
+    def draw_mountain(self):
+        """
+        draws the mountain
+        """
+        arcade.draw_polygon_filled(self.mountain_points, arcade.color.BROWN_NOSE)
+        # for p1, p2 in pairs(self.mountain_points):
+            # arcade.draw_line(p1.x, p1.y, p2.x, p2.y, arcade.color.BLACK, 3)
+            # arcade.draw_polygon_filled()
+
+    def draw_wall(self):
+        """Draws the wall on the left side of the screen."""
+        arcade.draw_line(*self.wall_points[0], *self.wall_points[1], arcade.color.BLACK, 10)
+
+    def draw_ball(self):
+        # center_x = self.ball.curr_pos[0]
+        # center_y = self.ball.curr_pos[1]
+        if self.ball is not None:
+            arcade.draw_circle_filled(*self.ball.curr_pos, Ball.radius, arcade.color.BLACK)
+
+    def draw_cannon(self) -> None:
+        arcade.draw_rectangle_filled(CANNON_X, CANNON_Y, 10, 50, arcade.color.BLACK, 90 - self.cannon_angle)
+
+    def fire_cannonball(self) -> None:
+        if self.ball is not None:
+            self.particle_system.particles.remove(self.ball)
+            del self.ball
+        print("Firing a new cannonball!")
+        print("Cannon angle:", self.cannon_angle)
+        magnitude = 85
+        x_component = magnitude * np.cos(np.deg2rad(self.cannon_angle)) * -1
+        y_component = magnitude * np.sin(np.deg2rad(self.cannon_angle))
+        velocity = (x_component, y_component)
+        print(velocity)
+        self.ball = Ball((CANNON_X, CANNON_Y), velocity)
+        self.particle_system.particles.append(self.ball)
+
 
 class ParticleType(Enum):
     ball = 0
@@ -141,18 +328,22 @@ class ParticleSystem():
         return (ball.curr_pos[0] - Ball.radius) - WALL_X < 5
 
     def ball_might_collide_with_mountain(self, ball: Ball) -> bool:
-        return (ball.curr_pos[0])
+        ball_x, ball_y = ball.curr_pos
+        radius = Ball.radius
+        return MOUNTAIN_X_MIN <= (ball_x + radius) and (ball_x - radius) <= MOUNTAIN_X_MAX \
+            and MOUNTAIN_Y_MIN <=(ball_y + radius) and (ball_y - radius) <= MOUNTAIN_Y_MAX
+
 
     def satisfy_constraints(self) -> None:
         for i in range(NUM_ITERATIONS):
             p: Particle
             for p in self.particles:
-
                 if p.particle_type == ParticleType.ball:
-                    if ball_might_collide_with_wall(p):
+                    if self.ball_might_collide_with_wall(p):
+                        print("ball might collide with wall!")
                         wall_point, ball_point = collision_detection.circle_line_intersection(p.curr_pos, Ball.radius)
-
-
+                    elif self.ball_might_collide_with_mountain(p):
+                        print("ball might collide with mountain!")
 
                     # constrain #1: the particles have to stay within the area.
                     min_constraint = (0, GROUND_Y)
@@ -209,3 +400,15 @@ class ParticleSystem():
 
 # if __name__ == '__main__':
 #     main()
+
+
+def main():
+    """ Main method """
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
+
