27acbbeeae7e29a8013ce0966628ead1c920e017
Fabrice <fabrice.normandin@gmail.com>
2018-11-20 23:13:21 -0500

    Made some real progress, enemies are there
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

---
+++ UnityAgents/AIAgents/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    public float movementSpeed = 1.0f;
+    CharacterController controller;
+    private void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += this.transform.forward * Time.deltaTime * this.movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enemy just collided with " + other.tag);
+        if (other.tag == "MiddleObstacle")
+        {
+
+        }
+    }
+}
---
+++ UnityAgents/AIAgents/Assets/Scripts/PortalBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PortalBehaviour : MonoBehaviour {
+    /// <summary>
+    /// Sound effect played when the portal is used.
+    /// </summary>
+    public AudioClip teleportSoundEffect;
+
+    /// <summary>
+    /// Other portal to teleport you to.
+    /// </summary>
+    public PortalBehaviour destination;
+
+
+    private AudioSource source;
+
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public static int SpawnOffset = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" || other.tag == "Enemy"){
+            var position = this.destination.transform.position;
+            var direction = this.destination.transform.forward;
+            var spawnPoint = position + SpawnOffset * direction;
+            spawnPoint.y = other.transform.position.y;
+
+            other.transform.SetPositionAndRotation(spawnPoint, this.destination.transform.rotation);
+
+
+            float vol = Random.Range(volLowRange, volHighRange);
+            source.PlayOneShot(teleportSoundEffect, vol);
+        }
+        else if (other.tag == "Agent")
+        {
+            Debug.Log("Agent is tryign to use the portal!");
+        }
+    }
+}
